bb77a3d86fdc9bd5182e5da3cfa997b572c1c0b8
kristo.josip <kristo.josip@nsoft.ba>
2020-04-07 22:15:09 +0200

    Rearrange app store

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="items-list-wrapper">
+    This is item list!
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'ItemsList',
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ItemsList></ItemsList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ItemsList from '@/components/ItemsList.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    ItemsList,
   },
 };
 </script>
