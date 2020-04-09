37b53887067246d45cebfd203ee98be818710e6b
kristo.josip <kristo.josip@nsoft.ba>
2020-04-09 11:48:20 +0200

    Fix mutation tests

--- tests/unit/actions.spec.js
+++ tests/unit/actions.spec.js
@@ -1,6 +1,6 @@
 import actions from '@/store/actions';
 import types from '@/store/mutationTypes';
-import { loadItemsList } from '@/api/';
+import { loadItemsList, fetchItemDetails } from '@/api/';
 import flushPromises from 'flush-promises';
 
 jest.mock('@/api/index');
@@ -17,5 +17,17 @@ describe('actions', () => {
       actions.loadItemsList(context);
       await flushPromises();
       expect(context.commit).toHaveBeenCalledWith(types.SET_ITEMS_LIST, items);
+    });
+  test('setItemDetails calls commit with result of setItemDetails',
+    async () => {
+      expect.assertions(1);
+      const itemDetails = {id: 1};
+      fetchItemDetails.mockImplementationOnce(() => Promise.resolve(itemDetails));
+      const context = {
+        commit: jest.fn(),
+      };
+      actions.setItemDetails(context);
+      await flushPromises();
+      expect(context.commit).toHaveBeenCalledWith(types.SET_ITEM_DETAILS, itemDetails);
     });
 });
--- tests/unit/mutations.spec.js
+++ tests/unit/mutations.spec.js
@@ -2,12 +2,12 @@ import mutations from '@/store/mutations';
 
 describe('mutations', () => {
   test('setItemsList sets state listItems to items', () => {
-    const items = [{id: 1}, {id: 2}];
+    const items = {data: [{id: 1}, {id: 2}]};
     const state = {
       listItems: [],
     };
     mutations.SET_ITEMS_LIST(state, items);
-    expect(state.listItems).toBe(items);
+    expect(state.listItems).toBe(items.data);
   });
   test('setItemDetails sets state itemDetails to details', () => {
     const details = {id: 1};
